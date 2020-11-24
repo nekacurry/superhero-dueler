6df9f55806c22d8305773c91cf63f052e4420e53
nekacurry <anneka498@gmail.com>
2020-11-23 19:52:37 -0600

    Create hero.py

---
+++ hero.py
@@ -0,0 +1,11 @@
+class Hero:
+
+  def __init__(self, name, starting_health=100):
+    self.name = name
+    self.starting_health = starting_health
+    self.current_health = starting_health
+
+if __name__ == "__main__":
+  my_hero = Hero("Grace Hopper", 200)
+  print(my_hero.name)
+  print(my_hero.current_health) 
