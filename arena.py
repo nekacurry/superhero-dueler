3c2c105d30ffb0b7f47bf093567c6aa973b8d0a4
nekacurry <anneka498@gmail.com>
2020-11-24 01:13:00 -0600

    Update arena.py

--- arena.py
+++ arena.py
@@ -100,3 +100,28 @@ class Arena:
     for hero in self.team_two.heroes:
         if hero.deaths == 0:
             print("survived from " + self.team_two.name + ": " + hero.name)
+
+if __name__ == "__main__":
+  game_is_running = True
+
+  # Instantiate Game Arena
+  arena = Arena()
+
+  #Build Teams
+  arena.build_team_one()
+  arena.build_team_two()
+
+  while game_is_running:
+
+      arena.team_battle()
+      arena.show_stats()
+      play_again = input("Play Again? Y or N: ")
+
+      #Check for Player Input
+      if play_again.lower() == "n":
+          game_is_running = False
+
+      else:
+          #Revive heroes to play again
+          arena.team_one.revive_heroes()
+          arena.team_two.revive_heroes()
