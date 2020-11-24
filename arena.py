a296bcbe7a69e5b3630d0c76298e824b96c7f89f
nekacurry <anneka498@gmail.com>
2020-11-24 00:54:27 -0600

    Create arena.py

---
+++ arena.py
@@ -0,0 +1,10 @@
+from ability import Ability
+from weapon import Weapon
+from armor import Armor
+from hero import Hero
+from team import Team
+
+class Arena:
+  def __init__(self):
+    self.team_one = None
+    self.team_two = None
