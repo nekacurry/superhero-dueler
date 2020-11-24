2b779db7db620f8cc9913044997b74f6083c7c61
nekacurry <anneka498@gmail.com>
2020-11-23 20:22:02 -0600

    Update armor.py

--- armor.py
+++ armor.py
@@ -4,7 +4,6 @@ class Armor:
   def __init__(self, name, max_block):
     self.name = name
     self.max_block = max_block
-  pass
 
   def block(self):
     random_value = random.randint(0, self.max_block)
